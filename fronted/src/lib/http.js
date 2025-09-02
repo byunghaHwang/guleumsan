ee6dcde350685683da586980892a5b1bf4a30328
FunFactory <manc9029@gamil.com>
2025-09-02 22:57:36 +0900

    하드코딩해서 확인하기

--- fronted/src/lib/http.js
+++ fronted/src/lib/http.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
-
 const http = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // dev/prod 자동 분기
-  withCredentials: true,                      // 세션/쿠키 쓰면 유지
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: 'https://funfactory.dev', // 임시 하드코딩
+  withCredentials: true,
+  headers: { 'Content-Type': 'application/json' },
 });
-
 export default http;
